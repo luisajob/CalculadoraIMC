11f60a931e459a6fa4ba85df3f72f08743dcf607
luisajob <luisajoblima@gmail.com>
2024-06-18 19:27:53 +0100

    Criado uma nova tela, e feito a classificacao do IMC aparecer para o Usuario

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.comunidadedevspace.imc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
@@ -21,13 +23,40 @@ class MainActivity : AppCompatActivity() {
         val btnCalcular = findViewById<Button>(R.id.btn_calcular)
 
         btnCalcular.setOnClickListener {
-            val peso: Float = edtPeso.text.toString().toFloat()
-            val altura: Float = edtAltura.text.toString().toFloat()
+            val pesoStr: String = edtPeso.text.toString()
+            val alturaStr: String = edtAltura.text.toString()
 
-            val alturaQ2 = altura * altura
-            val resultado = peso / alturaQ2
+            if (pesoStr.isEmpty() || alturaStr.isEmpty()) {
+                //Mostra mensagem ao usuario
+                Snackbar
+                    .make(
+                        edtPeso,
+                        "Prencha todos os campos",
+                        Snackbar.LENGTH_LONG
+                )
+                .show()
 
-            println("Seu IMC: "+ resultado)
+            } else {
+
+                    val peso = edtPeso.text.toString().toFloat()
+                    val altura = edtAltura.text.toString().toFloat()
+                    val alturaQ2 = altura * altura
+                    val resultado = peso / alturaQ2
+
+                //Navegar para a proxima tela
+                //Criar o layout da proxima tela
+                //Passar dados (resultado) para a proxima tela
+
+                //intent - Classe do proprio android
+                    val intent = Intent(this, Resultactivity::class.java)
+                    intent.putExtra(KEY_RESULT_IMC, resultado)
+                    startActivity(intent)
+
+                    println("Seu IMC: "+ resultado)
+                }
+
+
+
         }
     }
 }
